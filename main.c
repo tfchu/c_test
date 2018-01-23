3046f2e6bab7aac7242445d96102f3d91cc2c8a1
Tony Chu <tfchu@Tingfeng-Chus-MacBook-Pro.local>
2018-01-22 21:39:47 -0800

    update

--- main.c
+++ main.c
@@ -4,16 +4,28 @@
  *  Created on: Jun 3, 2017
  *      Author: tfchu
  */
+#include <stdio.h>
 //#include "memtest86p.h"
 #include "c_tutorial.h"
+#include "c_practice.h"
 
 int main(){
 	//printf("%lu", rand(0));
-	typedef_struct_test();
+	//typedef_struct_test();
 
 	//init_v();
 	//run_addr_tst1();
 	//run_modtst();
 	//run_movinv1();
+
+	// c_practice.c
+	rect_t r1; 
+	rect_t r2; 
+	rect_t r3; 
+	r1.left = -1; r1.bottom = -1; r1.right = 3; r1.top = 3; 
+	r2.left = 0; r2.bottom = 0; r2.right = 2; r2.top = 2; 
+	r3 = intersection(r1, r2);
+	printf("intersection is: (left) %f, (bottom) %f, (right) %f, (top) %f\n", r3.left, r3.bottom, r3.right, r3.top);
+
 	return 0;
 }
